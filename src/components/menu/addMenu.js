fea559cd8c70da583394ccb07d1198084da8ad9c
unso99 <unso99@naver.com>
2024-02-23 23:07:14 +0900

    Add: 키오스크 수정
    키오스크 위치 수정
    
    Resolves: #7

--- src/components/menu/addMenu.js
+++ src/components/menu/addMenu.js
@@ -1,12 +1,5 @@
 //src/components/Play.js
 
-import axios from "axios";
-import { useEffect, useRef, useState } from "react";
-import { Button, Container, Form } from "react-bootstrap";
-
-
-
-
 function AddMenu(){
  
 
