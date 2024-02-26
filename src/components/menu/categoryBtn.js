38b11546970ab9c2a254cc0a5800034d82b6508c
이름 <>
2024-02-27 04:01:42 +0900

    Modify: #15 메뉴 리스트 조회기능 깜빡이 현상 수정완료
    Resolves: #13 #14 #15
    Ref: #11 #12
    Related to: #18

--- src/components/menu/categoryBtn.js
+++ src/components/menu/categoryBtn.js
@@ -1,29 +1,65 @@
+import { click } from '@testing-library/user-event/dist/click';
+import { useEffect, useState } from 'react';
 import { DropdownButton } from 'react-bootstrap';
 import Dropdown from 'react-bootstrap/Dropdown';
+import { useSelector } from 'react-redux';
 
 /** 카테고리 드롭다운 버튼 */
 export default function CategoryBtn(props) {
 
-  /** Dropdown.Item 버튼을 눌렀을 때 onClick 함수를 통해 코드값을 이 변수에 전달 */
-  const handleDropdownItemClick = (categoryId) => {
-    props.onSelectCategory(categoryId);
-  };
+  const [categoryList, setCategory] = useState([])
+  const [checked, setChecked] = useState({
+    title:'전체'
+  })
 
+  /** 가져오기 전에 초기화 작업 */
+  const initialState = {
+    commonTable : []
+  }
+
+  /** 공통코드에서 카테고리 리스트 별도로 추출 */
+  const commonList = useSelector((state = initialState) => state) // state = {commonTable:[{},{},{}...]}
+
+  /** commonList가 업데이트될 때마다 newArray를 다시 계산하고 categoryList 상태를 업데이트 */
+  useEffect(() => {
+    const newArray = commonList.commonTable.filter(item => item.p_code_id === 1000);
+    setCategory(newArray);
+  }, [commonList]);
+
+  console.log(categoryList)
+
+  /** Dropdown.Item 버튼을 눌렀을 때 onClick 함수를 통해 코드값을 이 변수에 전달 */
+  const handleDropdownItemClick = (category) => {
+    if(category === 1000 || category === 0){
+      setChecked({
+        ...checked,
+        title:'전체'
+      })
+      props.onSelectCategory(category);
+    }else{
+      setChecked({
+        ...checked,
+        title:category.code_name
+      })
+      props.onSelectCategory(category);
+    }
+  };
+  
   return (
     <>
       <categoryBtn>
-          <DropdownButton id="dropdown-basic-button">
+        <DropdownButton id="dropdown-basic-button" title={checked.title}>
 
-            {/*메뉴 전체를 불러오기*/}
-            <Dropdown.Item value={0} onClick={() => handleDropdownItemClick(0)}>전체</Dropdown.Item>
+          {/* 메뉴 전체를 불러오기 */}
+          <Dropdown.Item onClick={() => handleDropdownItemClick(1000)}>전체</Dropdown.Item>
 
-            {/*항목별 메뉴 불러오기*/}
-            {props.list.map(item=>(
-              <Dropdown.Item key={item.code_id} value={item.code_id} 
-              onClick={() => handleDropdownItemClick(item.code_id)}
-              >{item.code_name}</Dropdown.Item>
-            ))}
-          </DropdownButton>
+          {/*항목별 메뉴 불러오기*/}
+          {categoryList.map(item => (
+            <Dropdown.Item key={item.code_id} onClick={() => handleDropdownItemClick(item)}>
+              {item.code_name}
+            </Dropdown.Item>
+          ))}
+        </DropdownButton>
       </categoryBtn>
     </>
   );
--- src/components/menu/main.js
+++ src/components/menu/main.js
@@ -6,44 +6,62 @@ import CategoryBtn from './categoryBtn';
 import { Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 
+/** HTML 본문 : 메뉴조회 전체 */ 
 function Main() {
 
-  /** 서버에서 가져온 데이터 배분 배열 변수 */ 
-  const [menuList, setMenuList] = useState([]);
   /** 로딩실행 값을 true 로 만들고 끝나면 false 로 변환하기 */
   const [loading, setLoading] = useState(true);
-
-  /** 처음 접속시 카테고리 정보를 담는 배분 배열 변수 */
-  const [categoryList,setCategoryList] = useState([]);
+  /** 서버에서 가져온 데이터 배분 배열 변수 */ 
+  const [menuList, setMenuList] = useState([]);
+  /** 필터링된 메뉴 데이터 배열 변수 */
+  const [filteredMenuList, setFilteredMenuList] = useState([]);
 
   const navigate= useNavigate()
-
-
+  
   /** 화면에 접속하자마자 서버로부터 메뉴정보를 모두 가져오는 코드를 작성 */
   useEffect(() => {
-    // 데이터를 서버로부터 가져오는 비동기 함수
-    const fetchData = async () => {
-      try {
-        /** 메뉴정보 전체 요청하기 */
-        const menuListResponse = await axios.post("/api/menu/list", {});
-        setMenuList(menuListResponse.data.list); // menuListResponse.data.list = [{},{},{}...]
-        /** 카테고리 요청하기 */
-        const categroyListResponse = await axios.post("/api/common/child", 1000,
-        { headers: { "Content-Type": "application/json" } })
-        setCategoryList(categroyListResponse.data.list) // categroyListResponse.data.list = [{},{},{}...]
+    const fetchData = () =>{
 
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      } finally {
-        // 데이터 로딩이 완료되면 로딩 상태를 false로 변경
-        setLoading(false);
-      }
-    };
-
+      /** 메뉴정보 전체 요청하기 */
+      axios.post("/api/menu/list", {})
+      .then(res=>{
+        const menuData = res.data.list; // menuListResponse.data.list = [{},{},{}...]
+        setMenuList(menuData)
+        setFilteredMenuList(menuData)
+      })
+      .catch(error=>{console.error('Error fetching data:', error);})
+      .finally(setLoading(false)) // 데이터 로딩이 완료되면 로딩 상태를 false로 변경
+    }
+    console.log(menuList)
     // fetchData 함수 호출
     fetchData();
-  }, []);
+  }, []); // [] 배열 안에 있는 값이 변화를 감지할 때 역시 함수가 호출됨
+
+    /** 카테고리 드롭다운 박스에 버튼을 눌렀을 때 그 값을 변수에 담는 함수이자 component 함수 연결고리 */
+    const handleCategoryChange = (item) => {
+      try{
+        if(item === 1000 || item === 0){
+
+          // 아무 작업 없이 원본 데이터를 그대로 유지
+          setFilteredMenuList(menuList);
+        }else{
   
+          // item.code_id와 일치하는 데이터만을 추려서 새로운 배열 생성
+          const filtered = menuList.filter(menu => menu.category_id === item.code_id);
+          // 새로운 배열을 상태로 설정
+          setFilteredMenuList(filtered);
+        }
+      }catch (e) {
+        console.error('Error handling category change:', e);
+      }
+      
+    };
+    
+    // 로딩 중일 때 Spinner를 보여줌
+    if (loading) {
+      return <Loading/>;
+    }
+
   //메뉴 수정 폼으로 가기
   const goToUpdateMenu = (MenuId)=>{
     navigate(`/menu/updateMenu/${MenuId}`)
@@ -53,45 +71,13 @@ function Main() {
     const goToAddMenu = () =>{
       navigate("/menu/addMenu")
   };
-  // 로딩 중일 때 Spinner를 보여줌
-  if (loading) {
-    return <Loading/>;
-  }
-
-    /** 카테고리 드롭다운 박스에 버튼을 눌렀을 때 그 값을 변수에 담는 함수이자 component 함수 연결고리 */
-    const handleCategoryChange = (categoryId) => {
-  
-      // 로딩 시작
-      setLoading(true);
-    
-      // fetchData 함수 내에서 selectedCategory 값을 직접 사용하지 않고 함수 파라미터로 받도록 수정
-      const fetchData = async (category) => {
-        try {
-          const menuListResponse = await axios.post("/api/menu/list", {
-            "category_id": category,
-          });
-          setMenuList(menuListResponse.data.list);
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        } finally {
-          // 데이터 로딩이 완료되면 로딩 상태를 false로 변경
-          setLoading(false);
-        }
-      };
-    
-      // fetchData 함수 호출 시 selectedCategory 값을 전달
-      fetchData(categoryId);
-    };
 
   /** 화면 구현 */
   return (
     <div>
 
-      {/* <AddMenuBtn></AddMenuBtn> */}
-      <CategoryBtn list={categoryList} onSelectCategory={handleCategoryChange} />
-
-      <Button onClick={goToAddMenu}>등록하기</Button>
-
+      <CategoryBtn onSelectCategory={handleCategoryChange}></CategoryBtn>
+      <Button variant="warning" onClick={goToAddMenu}>등록하기</Button>
 
       <Table striped>
         <thead>
@@ -104,13 +90,15 @@ function Main() {
           </tr>
         </thead>
         <tbody>
-          {menuList.map(item => (
+          {filteredMenuList.map(item => (
             <tr key={item.id}>
               <td>{item.category}</td>
               <td>{item.name}</td>
               <td>{item.price}</td>
               <td><button onClick={()=>goToUpdateMenu(item.id)}>수정</button></td>
-              <td><button>삭제</button></td>
+              <td><button onClick={()=>{
+                
+              }}>삭제</button></td>
             </tr>
           ))}
         </tbody>
@@ -118,18 +106,5 @@ function Main() {
     </div>
   );
 }
-
-
-// /** 등록 버튼 */
-
-// function AddMenuBtn() {
-//   return (
-//     <div>
-//       <addMenuBtn>
-//         <button>등록하기</button>
-//       </addMenuBtn>
-//     </div>
-//   );
-// }
 
 export default Main;
