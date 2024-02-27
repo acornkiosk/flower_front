64a7befedbce402c72a5f235d60dd14ded882662
unso99 <unso99@naver.com>
2024-02-27 16:02:46 +0900

    Add: 주문 상세 및 수정기능
    주문 상세 및 수정 삭제 기능 추가
    
    Resolves: #33
    Ref: #30

--- src/components/order/Order.js
+++ src/components/order/Order.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
-import { Button, CloseButton, Col, Container, FloatingLabel, Form, Modal, Row } from "react-bootstrap"
+import { Button, CloseButton, Col, Container, Modal, Row } from "react-bootstrap"
 import OrderItem from "./orderItem"
+import { useSelector } from "react-redux"
+import ConvertOptions from "./util"
 
 export default function Order() {
   //들어온 주문을 저장
@@ -9,10 +11,9 @@ export default function Order() {
   //상세 모달 state
   const [showModal, setShowModal] = useState(false)
   //상세용 데이터
-  const [data,setData] = useState([])
-  //상세 모달 handle
-  //화면 로딩시 
-  useEffect(() => {
+  const [data, setData] = useState([])
+  //refresh
+  const refresh = () => {
     //주문 db에 있는 정보 가져오기 
     axios.post("/api/order/list", {})
       .then(res => {
@@ -32,7 +33,14 @@ export default function Order() {
       }
       )
       .catch(error => console.log(error))
+  }
+  //화면 로딩시 
+  useEffect(() => {
+    refresh()
   }, [])
+
+  //모달에 있는 완료버튼을 누를 시 
+
   return (
     <div>
       <h1>주문 관리 페이지 입니다.</h1>
@@ -40,33 +48,54 @@ export default function Order() {
         <Container>
           <Row className="row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
             {Object.keys(orders).map(key =>
-              <Col key={key} onClick={() => {
-                setShowModal(true)
-                setData(orders[key])
-                }}>
-                <OrderItem orders={orders[key]} setOrders={setOrders} list={orders} id={key} />
+              <Col key={key}>
+                <OrderItem orders={orders[key]} setOrders={setOrders} list={orders} id={key} setShowModal={setShowModal} setData={setData} />
               </Col>
             )}
           </Row>
         </Container>
       </div>
-      <DetailModal show={showModal} data={data} setShowMoadl = {setShowModal} onHide = {() => setShowModal(false)}/>
+      <DetailModal show={showModal} data={data} setShowModal={setShowModal} refresh={refresh} onHide={() => setShowModal(false)} />
     </div>
   )
 }
 
 function DetailModal(props) {
-  console.log(props.data)
   let order_id
   let kiosk_id
-  if(props.data.length !== 0){
+  if (props.data.length !== 0) {
     order_id = props.data[0].order_id
     kiosk_id = props.data[0].kiosk_id
   }
-  
+  //완료버튼을 누를시 
+  function onCompleted() {
+    for (let item of props.data) {
+      item.is_completed = 'true'
+      axios.post("/api/order/update", item)
+        .then(res => {
+          if (res.data.status === 'OK') {
+            props.refresh()
+          }
+        })
+        .catch(error => console.log(error))
+    }
+    props.setShowModal(false)
+  }
+
+  //주문취소 버튼을 누를시
+  function onDelete() {
+    axios.post("/api/order/deleteAll", { order_id: order_id })
+      .then(res => {
+        if (res.data.status === 'OK') {
+          props.refresh()
+          props.setShowModal(false)
+        }
+      })
+  }
+
   return (
     <Modal
-      show = {props.show}
+      show={props.show}
       size="lg"
       centered
     >
@@ -75,17 +104,28 @@ function DetailModal(props) {
           <Row className="justify-content-between">
             <Col>{order_id}번 주문 내역</Col>
             <Col>키오스크 : {kiosk_id}번</Col>
-            <Col className="text-end"><CloseButton onClick={() =>{
+            <Col className="text-end"><CloseButton onClick={() => {
               props.setShowMoadl(false)
             }}></CloseButton></Col>
           </Row>
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
+        {props.data.map(item =>
+          <>
+            <Row className="mb-3 ms-3">
+              <Col>
+                <Row>{item.menu_name} X {item.menu_count}</Row>
+                <Row>{ConvertOptions(item.options)}</Row>
+              </Col>
+              <Col>주문 시간 : {item.regdate}</Col>
+            </Row>
+          </>
+        )}
       </Modal.Body>
       <Modal.Footer>
-        <Button>완료</Button>
-        <Button>주문 취소</Button>
+        <Button onClick={onCompleted}>완료</Button>
+        <Button onClick={onDelete}>주문 취소</Button>
       </Modal.Footer>
     </Modal>
   );
