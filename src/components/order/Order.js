319aca2252941f8bf839aeae92bb5e26bde7e5da
unso99 <unso99@naver.com>
2024-02-27 10:13:55 +0900

    Refactor: 키오스크 관리 중복코드 개선

--- src/components/order/Order.js
+++ src/components/order/Order.js
@@ -1,12 +1,16 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
-import { Col, Container, Row } from "react-bootstrap"
+import { Button, CloseButton, Col, Container, FloatingLabel, Form, Modal, Row } from "react-bootstrap"
 import OrderItem from "./orderItem"
 
-function Order() {
+export default function Order() {
   //들어온 주문을 저장
   const [orders, setOrders] = useState({})
-
+  //상세 모달 state
+  const [showModal, setShowModal] = useState(false)
+  //상세용 데이터
+  const [data,setData] = useState([])
+  //상세 모달 handle
   //화면 로딩시 
   useEffect(() => {
     //주문 db에 있는 정보 가져오기 
@@ -36,17 +40,53 @@ function Order() {
         <Container>
           <Row className="row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
             {Object.keys(orders).map(key =>
-              <Col key={key}>
+              <Col key={key} onClick={() => {
+                setShowModal(true)
+                setData(orders[key])
+                }}>
                 <OrderItem orders={orders[key]} setOrders={setOrders} list={orders} id={key} />
               </Col>
             )}
           </Row>
         </Container>
-
       </div>
-
+      <DetailModal show={showModal} data={data} setShowMoadl = {setShowModal} onHide = {() => setShowModal(false)}/>
     </div>
   )
 }
 
-export default Order
+function DetailModal(props) {
+  console.log(props.data)
+  let order_id
+  let kiosk_id
+  if(props.data.length !== 0){
+    order_id = props.data[0].order_id
+    kiosk_id = props.data[0].kiosk_id
+  }
+  
+  return (
+    <Modal
+      show = {props.show}
+      size="lg"
+      centered
+    >
+      <Modal.Header className="d-flex">
+        <Modal.Title className="flex-fill">
+          <Row className="justify-content-between">
+            <Col>{order_id}번 주문 내역</Col>
+            <Col>키오스크 : {kiosk_id}번</Col>
+            <Col className="text-end"><CloseButton onClick={() =>{
+              props.setShowMoadl(false)
+            }}></CloseButton></Col>
+          </Row>
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button>완료</Button>
+        <Button>주문 취소</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
