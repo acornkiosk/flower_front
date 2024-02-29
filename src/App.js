6c0fa836230fecaef28d228933963d6f0bf8a405
scott <hilnmyy12@naver.com>
2024-02-29 15:12:06 +0900

    로그인 기능 구현중

--- src/App.js
+++ src/App.js
@@ -11,6 +11,11 @@ import User from './components/user/User';
 
 function App() {
   const [common,setCommon] = useState([])
+  const [isLogin, setIsLogin] = useState(false);
+  const handleLogin = () => {
+    setIsLogin(true);
+  };  
+
   return (
     <div className="container">
       <h1>인덱스 테스트 페이지 입니다</h1>
@@ -19,14 +24,15 @@ function App() {
         <li><Link to="/order">주문 관리</Link></li>
         <li><Link to="/user">사용자 관리</Link></li>
         <li><Link to="/menu">메뉴 관리</Link></li>
-        <li><Link to="/login">로그인</Link></li>
+        {isLogin ? null : <li><Link to="/login">로그인</Link></li>}
       </ul>
       <Routes>
+        <Route path="/" Component={null} />
         <Route path='/kiosk' Component={Kiosk}/>
         <Route path='/order' Component={Order}/>
         <Route path='/user' Component={User}/>
         <Route path='/menu' Component={Menu}/>
-        <Route path='/login' Component={Login}/>
+        <Route path='/login' element={<Login isLogin={isLogin} handleLogin={handleLogin} />} />
       </Routes>
       <p>spring boot 와 연동이 되었는지 테스트 입니다.</p>
       <button onClick={() => {
@@ -37,13 +43,22 @@ function App() {
             setCommon(res.data.list)
           })
       }}>응답받기</button>
+      <button onClick={()=>{
+        axios.get("/test")
+        .then(res=>{
+          console.log(res.data)
+        })
+      }}> test
+      </button>
       <table>
         <thead>
-          <th>code_id</th>
-          <th>p_code_id</th>
-          <th>code_name</th>
-          <th>code_value</th>
-          <th>code_img</th>
+          <tr>
+            <th>code_id</th>
+            <th>p_code_id</th>
+            <th>code_name</th>
+            <th>code_value</th>
+            <th>code_img</th>
+          </tr>
         </thead>
         <tbody>
           {common.map(item =>
--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+// App.js 를 import 해서 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//라우터를 사용할 준비
 import { BrowserRouter } from 'react-router-dom';
+// legacy_createStore 를 createStore 라는 이름으로 사용하기 (store 를 만들 함수)
+import { legacy_createStore as createStore } from 'redux';
+// store(저장소) 공급자 
+import { Provider } from 'react-redux';
 
+//store 에서 관리될 초기 상태값
+const initialState={
+  userName:null,
+  isLogin:false
+}
+
+//reducer 함수
+const reducer = (state=initialState, action)=>{
+  let newState 
+  //state 와 action 을 전달 받아서 새로운 state 를 리턴해 주면 된다
+  if(action.type === "UPDATE_USER"){
+    newState = {
+      ...state,
+      userName:action.payload
+    }
+  }else if(action.type === "SET_LOGIN"){
+    newState ={
+      ...state,
+      isLogin:action.payload //boolean 값으로 전달될 예정 
+    }
+  }else{
+    newState=state
+  }
+  return newState
+}
+//reducer 함수를 인자로 전달하면서 store(저장소) 를 만들어준다 
+const store = createStore(reducer)
+
+//id 가 root 인 곳에 UI 출력하기 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
     </BrowserRouter>
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
