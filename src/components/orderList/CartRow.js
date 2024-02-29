527dcd69132a64ee4f74d55cacfc74d3ba5303ab
unso99 <unso99@naver.com>
2024-02-29 12:50:37 +0900

    Add: 장바구니 옵션 조회 기능 추가
    Ref: #48

--- src/components/orderList/CartRow.js
+++ src/components/orderList/CartRow.js
@@ -3,7 +3,7 @@ import { DashSquare, PlusSquare, XSquareFill } from "react-bootstrap-icons"
 import { useDispatch, useSelector } from "react-redux"
 
 export default function CartRow(props) {
-  //orders에 값 변경하기
+  const commonTable = useSelector(state => state.commonTable)
   const orders = useSelector(state => state.orders)
   const dispatch = useDispatch()
   const { item } = props
@@ -41,6 +41,19 @@ export default function CartRow(props) {
     })
     updateOrders(newOrders)
   }
+  //options 이름으로 변경 기능
+  const convertOptions = () => {
+    let result = ""
+    let list = item.options.replace(" ","").split(",")
+    list.forEach(option => {
+      commonTable.forEach(common => {
+        if(parseInt(option) === common.code_id){
+          result += common.code_name + " "
+        }
+      })
+    })
+    return result
+  }
 
   return (
     <>
@@ -48,7 +61,7 @@ export default function CartRow(props) {
         <Col md={1} className="mt-1 md-1">
           <Button variant="secodary" onClick={deleteItem}><XSquareFill /></Button>
         </Col>
-        <Col md={5} className="mt-1 mb-1 d-flex align-items-center">{item.menu_name} {item.options}</Col>
+        <Col md={5} className="mt-1 mb-1 d-flex align-items-center">{item.menu_name} {convertOptions()}</Col>
         <Col md={1} className="mt-1 mb-1 d-flex align-items-center"><Button variant="warning" onClick={plus}><PlusSquare /></Button></Col>
         <Col md={1} className="mt-1 mb-1 d-flex align-items-center">{item.menu_count}개</Col>
         <Col md={1} className="mt-1 mb-1 d-flex align-items-center"><Button variant="warning" onClick={minus}><DashSquare /></Button></Col>
