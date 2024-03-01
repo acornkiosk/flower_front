fd050692dbc3bc7ed95b77058cb5d81f48b7c582
anchul <lacjesusg@gmail.com>
2024-03-01 16:12:50 +0900

    login page 디자인중

--- src/components/login/OwnerManage.js
+++ src/components/login/OwnerManage.js
@@ -1,14 +1,13 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Button, Modal, Table } from "react-bootstrap";
-import { useDispatch, useSelector } from "react-redux";
 
 
 
 export default function OwnerMange() {
 
      //사장님(owner) 리스트 관리
-     const [ownerlist,setOwnerlist] =useState([]);
+    const [ownerlist,setOwnerlist] =useState([]);
    
     //화면 refresh 하기
     const refresh=()=>{
@@ -30,12 +29,11 @@ export default function OwnerMange() {
    useEffect(()=>{
         refresh()
     },[])
-    //추가 modal 관리
-    const addmodal=useSelector(state=>state.addmodal)  
-    //수정 modal
-    const upmodal=useSelector(state=>state.upmodal)
-    const dispatch=useDispatch()
-    const [upid,setUpid]=useState({});
+    
+    const [showAddModal, setShowAddModal] = useState(false)
+    const [showUpModal, setShowUpModal] = useState(false)
+    const [currentItem, setCurrentItem] = useState(null)
+
     //사장님 삭제
     const ownerDelete=(id)=>{
         axios.delete("/super/ownerDelete/"+id,{
@@ -58,8 +56,10 @@ export default function OwnerMange() {
             <h1>super 전용 (관리자 모드)</h1>
 
             <h4>사장님(OWENR) 목록</h4>
-            <button onClick={()=>{dispatch({type:"UPDATE_AddMODAL", payload:true})}}> 사장님 추가</button>
-            <AddModal show={addmodal} refresh={refresh} />
+            <button onClick={()=>{
+                setShowAddModal(true)
+            }}> 사장님 추가</button>
+            <AddModal show={showAddModal} refresh={refresh} setshow={setShowAddModal}/>
             <Table>
                 <thead>
                     
@@ -83,15 +83,14 @@ export default function OwnerMange() {
                             <td>{item.role}</td>
                             <td>{item.regdate}</td>
                             <td>
-                            <Button variant="warning" onClick={()=>{
-                            dispatch({type:"UPDATE_UpMODAL", payload:true });
-                            setUpid(item.id)
-                            }}>
-                                수정
-                            </Button>    
-                            </td>
-                            <UpModal show={upmodal} refresh={refresh} id={upid}/>  
- 
+                                <Button variant="warning" onClick={()=>{
+                                   setShowUpModal(true)
+                                    //setUpid(item.id)
+                                    setCurrentItem(item)
+                                }}>
+                                    수정
+                                </Button>    
+                            </td>  
                             <td><Button variant="danger" onClick={()=>{
                                 if(window.confirm("삭제할거냐?")){
                                     ownerDelete(item.id)
@@ -103,12 +102,13 @@ export default function OwnerMange() {
                     }
                 </tbody>
             </Table>
+            <UpModal show={showUpModal} setshow={setShowUpModal} item={currentItem} refresh={refresh} />
         </div>
     );
 }
 
 function AddModal(props) {
-    const dispatch=useDispatch()
+    //여기서 모달을 숨기고 싶으면 props.setshow(false) 가 실행되면 된다
 
     // 사장(owner) 추가
     const ownerInsert = () => {
@@ -121,7 +121,7 @@ function AddModal(props) {
             .then(res => {
                 console.log(res.data);
                 alert(res.data+"님(owenr) 등록 되었습니다.")
-                dispatch({type:"UPDATE_AddMODAL", payload:false})
+                props.setshow(false)
                 props.refresh() 
             })
             .catch(error => {
@@ -161,20 +161,21 @@ function AddModal(props) {
         <Modal.Footer>
        
           <Button onClick={ownerInsert}>등록</Button>
-          <Button onClick={()=>{dispatch({type:"UPDATE_AddMODAL", payload:false})}} >Close</Button>
+          <Button onClick={()=>{props.setshow(false)}} >Close</Button>
         </Modal.Footer>
       </Modal>
     );
   }
 
   function UpModal(props) {
-    const dispatch = useDispatch();
      //state로 값을 관리
-    const [owner, setOwner] = useState({
-        id:"",
-        userName:"",
-        newId:""
-    });
+     const [owner, setOwner] = useState( {
+         id: ""
+         , newId: ""
+         , userName: "" 
+        });
+
+    console.log(props.item)
     //모달창에 입력값 바뀌면 state 값 바꾸기
     const handleChange = (e)=>{
         setOwner({
@@ -184,26 +185,10 @@ function AddModal(props) {
         })
         console.log("변경")
     }
-    //해당 사장 정보 불러오기
-    
-    useEffect(()=>{
-        axios.post("/super/getOwner",props.id, {
-            headers: {
-                Authorization: localStorage.token
-            }
-        })
-        .then(res => {
-            setOwner(res.data); // 단일 객체가 아닌 여러 개의 속성을 가진 객체를 업데이트 해야 함
-        })
-        .catch(error => {
-            console.log(error);
-        });
-    },[props.id])
-   
 
    
     const handleSave = ()=>{
-        axios.put("/super/ownerUpdate/"+props.id,owner,{
+        axios.put("/super/ownerUpdate/"+owner.id,owner,{
             headers: {
                 "Content-Type": "application/json",
                 Authorization: localStorage.token
@@ -213,7 +198,7 @@ function AddModal(props) {
             //회원 목록 보기로 이동
             console.log(res.data)
             props.refresh()
-            dispatch({type:"UPDATE_UpMODAL", payload:false})
+            props.setshow(false)
         })
         .catch(error=>{
             console.log(error)
@@ -229,21 +214,21 @@ function AddModal(props) {
       >
         <Modal.Header>
           <Modal.Title id="contained-modal-title-vcenter">
-          {owner.id} 사장님
+          {props.item.id} 사장님
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
         <label htmlFor="newId">새로운 아이디</label>
         <input type="text" name="newId"  onChange={handleChange} />
         <label htmlFor="userName">이름</label>
-        <input type="text" name="userName" value={owner.userName} onChange={handleChange} />
+        <input type="text" name="userName" value={props.item.userName} onChange={handleChange} />
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={() => {
             handleSave()
           }}>수정</Button>
           <Button onClick={() => {
-            dispatch({ type: "UPDATE_UpMODAL", payload: false })
+          props.setshow(false)
           }}>취소</Button>
         </Modal.Footer>
       </Modal>
