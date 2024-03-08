76af218d5cdd9547b98d54aed655bf501620e25d
anchul <lacjesusg@gmail.com>
2024-03-08 17:09:00 +0900

    토큰 만료시 로그아웃 처리 및 login.js 리팩토링

--- src/index.js
+++ src/index.js
@@ -21,29 +21,48 @@ let rank=null
 
 axios.defaults.baseURL = process.env.PUBLIC_URL
 
-function test(){
-  if (localStorage.token) {
-    
-    //토큰을 디코딩
-    const result = decodeToken(localStorage.token);
-    //초단위
-    const expTime = result.payload.exp * 1000; // *1000 을 해서 ms 단위로 만들고 
-    //현재시간
-    const now = new Date().getTime();
-    if(expTime > now){
-      userName=result.payload.sub
-      isLogin=true
-      rank=result.payload.rank
-      //axios 의 header 에 인증정보를 기본으로 가지고 갈수 있도록 설정 
-      axios.defaults.headers.common["Authorization"]="Bearer+"+localStorage.token
-    }else{
-      //만료된 토큰은 삭제한다 
-      delete localStorage.token
-      alert("토큰이 만료되었습니다.") 
-      window.location.replace("/")
-    }
+
+if (localStorage.token) {
+  
+  //토큰을 디코딩
+  const result = decodeToken(localStorage.token);
+  //초단위
+  const expTime = result.payload.exp * 1000; // *1000 을 해서 ms 단위로 만들고 
+  //현재시간
+  const now = new Date().getTime();
+  if(expTime > now){
+    userName=result.payload.sub
+    isLogin=true
+    rank=result.payload.rank
+    //axios 의 header 에 인증정보를 기본으로 가지고 갈수 있도록 설정 
+    axios.defaults.headers.common["Authorization"]="Bearer+"+localStorage.token
+  }else{
+    //만료된 토큰은 삭제한다 
+    delete localStorage.token
+    alert("토큰이 만료되었습니다.") 
+    window.location.replace("/")
   }
 }
+let timeoutId
+const checkTokenTimeout = ()=>{
+  if(!localStorage.token)return;
+  //토큰을 디코딩
+  const result = decodeToken(localStorage.token);
+  //초단위
+  const expTime = result.payload.exp * 1000; // *1000 을 해서 ms 단위로 만들고 
+  //현재시간
+  const now = new Date().getTime();
+  //남은 유효기간
+  let remain=expTime-now
+  //남은 시간이 경과하면 호출되는 함수 등록
+  timeoutId = setTimeout(()=>{
+    delete localStorage.token
+    alert("토큰이 만료되었습니다.") 
+    window.location.replace("/")
+  }, remain)
+}
+
+checkTokenTimeout()
 
 const initialstate={
   userName,
@@ -54,7 +73,6 @@ const initialstate={
 }
 
 
-setInterval(test,3600*1000)
 const reducer=(state=initialstate,action)=>{
   let newState
 
@@ -63,21 +81,16 @@ const reducer=(state=initialstate,action)=>{
       ...state,
       commonTable : action.payload
     }
-  }else if(action.type === "UPDATE_USER"){
-    newState = {
-      ...state,
-      userName:action.payload
-    }
-  }else if(action.type === "SET_LOGIN"){
+  }else if(action.type==="SET_LOGIN"){
+    console.log(action.payload)
     newState ={
       ...state,
-      isLogin:action.payload //boolean 값으로 전달될 예정 
+      userName:action.payload.userName
+      ,isLogin:action.payload.isLogin
+      ,rank:action.payload.rank
     }
-  }else if(action.type === "SET_RANK"){
-    newState ={
-      ...state,
-      rank:action.payload 
-    }
+    if(timeoutId)clearTimeout(timeoutId)
+    checkTokenTimeout()
   }else{
     newState=state
   }
