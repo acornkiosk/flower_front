c5f6267b3aa7aa86c02b8cf364a12d6cfd3bf279
이름 <>
2024-03-11 01:04:29 +0900

    주문기능 구현중

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -7,8 +7,7 @@ import OrderItem from "../components/order/orderItem"
 /** 웹소켓 계획
  * 1. 들어온 주문 개수만큼 sidebar.js 개수 표시
  * 2. 다른 페이지에 있더라도 새로운 주문이 들어올 때마다 알림표시 
- * 3. 키오스크 관리에서 off 하면 '점검중'으로 표기해서 주문이 불가하도록 만들기
- * 4. 주문완료 진행시 손님 키오스크에 알림표시, 번호표기 
+ * 3. 주문완료 진행시 손님 키오스크에 알림표시, 번호표기 
  */
 export default function Order() {
   //들어온 주문을 저장
@@ -19,16 +18,7 @@ export default function Order() {
   const [data, setData] = useState([])
   /** 이것만 있으면 웹소켓 ID를 유지한 채로 사용가능함! */
   const ws = useSelector((state) => state.ws)
-  useEffect(() => {
-    connect()
-  }, [])
-  const connect = () => {
-    if(ws !== null){
-      ws.send("보냄")
-    }
-  }
-  //refresh
-  const refresh = () => {
+  function getOrders(){
     //주문 db에 있는 정보 가져오기 
     axios.post("/api/order/list", {})
       .then(res => {
@@ -52,6 +42,61 @@ export default function Order() {
         console.log(error)
       })
   }
+  /** 화면 호출시 */
+  useEffect(() => {
+    connect()
+  }, [])
+  /** 웹소켓 연결함수 */
+  const connect = () => {
+    /** 웹브라우저 새로고침 이후로 정보를 확인하기 위해 if문 작성 */
+    if (ws == null) { console.log("주문관리 : 웹소켓 정보 => 없음") }
+    ws.onopen = () => {
+      console.log("주문관리 : 실시간 화면연동 시작(웹소켓)")
+      getOrders()
+    }
+    ws.onerror = () => {
+      console.log("주문관리 : 화면 연동이 원활하게 이루어지지 않고 있습니다. 서버 확인이 필요합니다(웹소켓)")
+      /** 연결 다시시도 */
+      setTimeout(() => {
+        connect()
+      }, 5000)
+    }
+    ws.close = () => { ws.onopen() }
+    /** flower_kiosk 의 Cart.js 에서 주문이 들어왔을 때 동작된다. */
+    ws.onmessage = (msg) => {
+      if (msg != null) {
+        var result = JSON.parse(msg.data);
+        if (result.type === "UPDATE_ORDERS") getOrders()
+      } else {
+        console.log("주문없엉")
+      }
+    }
+  }
+  //refresh
+  // const refresh = () => {
+  //   //주문 db에 있는 정보 가져오기 
+  //   axios.post("/api/order/list", {})
+  //     .then(res => {
+  //       const orderData = res.data.list
+  //       //order_id를 기준으로 주문들을 묶어서 저장할 객체
+  //       let updatedOrders = {}
+  //       orderData.forEach(order => {
+  //         const orderId = order.order_id
+  //         if (updatedOrders[orderId]) {
+  //           /** 동일한 정보가 있는 경우는 덮어쓰기 */
+  //           updatedOrders[orderId].push(order)
+  //         } else {
+  //           /** 새로운 정보는 추가하기 */
+  //           updatedOrders[orderId] = [order]
+  //         }
+  //       })
+  //       setOrders(updatedOrders)
+  //     })
+  //     .catch(error => {
+  //       console.log("주문관리 : 400이 나올 경우 서버 상태와 주문개수 확인")
+  //       console.log(error)
+  //     })
+  // }
   return (
     <div>
       <h1>주문 관리</h1>
@@ -66,7 +111,7 @@ export default function Order() {
           </Row>
         </Container>
       </div>
-      <DetailModal show={showModal} data={data} setShowModal={setShowModal} refresh={refresh} onHide={() => setShowModal(false)} />
+      <DetailModal show={showModal} data={data} setShowModal={setShowModal} getOrders={getOrders} onHide={() => setShowModal(false)} />
     </div>
   )
 }
