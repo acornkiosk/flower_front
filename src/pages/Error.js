28dceb1dd5192c25f7abad34eeabdd7587abce03
anchul <lacjesusg@gmail.com>
2024-03-12 17:09:14 +0900

    error.js 페이지 디자인

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,6 +1,5 @@
-import { red } from "@mui/material/colors"
-import { color } from "d3-color"
 import { useEffect } from "react"
+import { useSelector } from "react-redux"
 import { useNavigate } from "react-router"
 
 
@@ -11,13 +10,22 @@ export default function Error(){
             navigate("/")
         }, 3000)
     },[])
-   
+    const userName=useSelector(state=>state.userName)
+    const rank = useSelector(state => state.rank)
+    let rankText = "";
+    if (rank === 3001) {
+      rankText = "[관리자]";
+    } else if (rank === 3002) {
+      rankText = "[사장님]";
+    } else if (rank === 3003) {
+      rankText = "[매니저]";
+    } else if (rank === 3004) {
+      rankText = "[직원]";
+    }
     return(
         <div>
-          <h1 style={{ color: 'red' }}>
-            권한이 부족하여 이 작업을 수행할 수 없습니다.
-            </h1>
-          <img src="/images/test.jpg" style={{ width: '100%' }}  />
+         <h1 style={{textAlign:'center'}}> <strong>{userName}</strong>{rankText} 님 권한에 맞지 않는 페이지 요청을 하였습니다!</h1>
+          <img src="/images/error.png" style={{ width: '100%' ,height:"720px"}}  />
         </div>
     )
 }
