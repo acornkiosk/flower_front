2e42474a67bec5686391f61141daaed053186ea2
unso99 <unso99@naver.com>
2024-03-17 13:31:20 +0900

    Refactor: 코드 정리

--- src/components/kiosk/AddModal.js
+++ src/components/kiosk/AddModal.js
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
-import { Button, Col, Form, InputGroup, Modal, Row } from "react-bootstrap"
+import { Button, Col, Form, InputGroup, Modal, Row } from "react-bootstrap";
 
 export default function AddModal(props) {
-  const {addModalShow,setAddModalShow,combinedHandleChange, addKiosk, isLocationValid} = props
+  const { addModalShow, setAddModalShow, combinedHandleChange, addKiosk, isLocationValid } = props
   const [currentLocation, setCurrentLocation] = useState("");
 
   // 모달이 열릴 때마다 위치 초기화
@@ -14,19 +14,19 @@ export default function AddModal(props) {
 
   return (
     <>
-      <Modal size="lg" centered show={addModalShow} onHide={() => {setAddModalShow(false) }}>
+      <Modal size="lg" centered show={addModalShow} onHide={() => { setAddModalShow(false) }}>
         <Modal.Header closeButton>
           <Modal.Title>키오스크 추가</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <InputGroup className="mb-3">
             <InputGroup.Text>키오스크 위치</InputGroup.Text>
-            <Form.Control type="text" placeholder="키오스크 위치 입력" value={currentLocation} 
-              onChange={(e)=>{
+            <Form.Control type="text" placeholder="키오스크 위치 입력" value={currentLocation}
+              onChange={(e) => {
                 setCurrentLocation(e.target.value); // 현재 위치 상태 업데이트
                 combinedHandleChange(e);
-              }} 
-              disabled={!addModalShow}/>
+              }}
+              disabled={!addModalShow} />
           </InputGroup>
         </Modal.Body>
         <Modal.Footer>
