6971f4b9bce01678b89dc1cf89a42c0f8c0d59e0
unso99 <unso99@naver.com>
2024-03-18 15:14:55 +0900

    Refator: 주문관리 웹소켓
    웹소켓 연동 간소화 및 정리
    
    Ref: #94

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import OrderToast from './components/toast/OrderToast';
 import Navbar from './components/toolbar/NavBar';
 import Sidebar from './components/toolbar/SideBar';
 import DashBoard from './pages/DashBoard';
@@ -13,14 +14,11 @@ import User from './pages/User';
 import Kiosk from './pages/kiosk';
 import Login from './pages/login';
 import Menu from './pages/menu';
-import style from "./components/order/style"
-import Toast from 'react-bootstrap/Toast';
 
 function App() {
   /** 주문정보 들어오면 바로 Order.js 로 신호 전달하기 */
   const [isOrdered, setIsOrdered] = useState(false)
-  /** 주문왔을 때 띄울 toast */
-  const [isToast, setIsToast] = useState(false)
+  const isToast = useSelector(state => state.isToast)
   const dispatch = useDispatch()
   useEffect(() => {
     axios.post("/api/common/child", { code_id: 0 })
@@ -33,38 +31,6 @@ function App() {
       })
       .catch(error => console.log(error))
   }, [])
-  /** 웹소켓 참조값을 담을 필드 */
-  let ws = useSelector((state) => state.ws)
-  /** 화면 실행시 */
-  useEffect(() => {
-    /** 웹브라우저 새로고침 대응 */
-    if (ws === null || ws === undefined) {
-      console.log("App.js: ws 객체 dispatch 중 ")
-      const action = { type: "SET_WEBSOCKET" }
-      dispatch(action)
-    }else{
-      /** 웹소켓 컨넥트 확인용 : ws.readyState */
-      console.log("웹소켓 컨넥트 (App.js): " + ws.readyState)  
-      connect(ws)
-    }
-  }, [ws])
-  /** 손님 키오스크로부터 오는 모든 메시지를 전달받음 */
-  const connect = (ws) => {
-    if(ws !== undefined){
-      ws.onmessage = (msg) => {
-        if (msg != null) {
-          var result = JSON.parse(msg.data);
-          if (result.type === "UPDATE_ORDERS_TOAST"){
-            console.log(result.type)
-            setIsToast(true)
-          }else if (result.type === "UPDATE_ORDERS"){   
-            console.log(result.type)
-            setIsOrdered(true)
-          }
-        }
-      }
-    }
-  }
   return (
     <div className="d-flex">
       <div>
@@ -86,31 +52,10 @@ function App() {
             </Routes>
           </div>
         </div>
-        {isToast && <OrderToast show={isToast} setIsToast={setIsToast}/>}
+        {isToast && <OrderToast />}
       </div>
     </div>
   );
 }
 
 export default App;
-
-/** 실험중 */
-function OrderToast(props) {
-
-  const { setIsToast } = props
-
-  return (
-    <Toast 
-    show={props.show} 
-    style={style.orderMessageToast} 
-    onClose={() => setIsToast(false)} 
-    delay={3000} autohide
-    variant="success">
-      <Toast.Header>
-        <strong className="me-auto">매출상승!</strong>
-        <small>일하자</small>
-      </Toast.Header>
-      <Toast.Body>새로운 주문이 들어왔습니다</Toast.Body>
-    </Toast>
-  );
-}
--- src/components/toolbar/NavBar.js
+++ src/components/toolbar/NavBar.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import { CDBNavbar } from "cdbreact";
-import React from "react";
+import React, { createRef } from "react";
 import { Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router";
+import { close } from '../../util/websocket';
 import { Header } from './NavBar.Style';
 
 const Navbar = () => {
@@ -12,9 +13,10 @@ const Navbar = () => {
   const rank = useSelector(state => state.rank)
   const navigate = useNavigate();
   const dispatch = useDispatch();
-
+  const ws = useSelector(state => state.ws)
   //로그아웃 핸들러
   const handleLogout = () => {
+    close(ws, "로그아웃")
     //localStorage 에 저장된 토큰 삭제
     delete localStorage.token
     //store 의 상태 바꾸기
@@ -23,7 +25,8 @@ const Navbar = () => {
       userName: null,
       isLogin: false,
       rank: null,
-      role: []
+      role: [],
+      websocket: createRef()
     }
     dispatch({ type: "SET_LOGIN", payload: data })
     navigate("/")
@@ -45,16 +48,13 @@ const Navbar = () => {
     <Header style={{ background: "#333", color: "#fff", minHeight: '73.5px', display: 'flex', justifyContent: 'end', alignItems: 'center' }}>
       <CDBNavbar dark expand="md" scrolling >
         <div className="ml-auto">
-
           {
             isLogin && <i className="mx-4"><strong>{rankText} {userName} </strong>님 로그인중 <Button variant="secondary" className='ms-3' onClick={handleLogout}>로그아웃</Button></i>
           }
-
           {
             !isLogin && <Button variant="secondary" className="mx-4" onClick={() => {
               navigate("/login")
             }}> 로그인 </Button>
-
           }
         </div>
       </CDBNavbar>
