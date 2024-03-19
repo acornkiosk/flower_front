65c579c495495f67af99fd9a65679318a148345d
scott <hilnmyy12@gmail.com>
2024-03-19 18:28:09 +0900

    Add : 직원 추가 유효성 추가
    Resolves : #232
    Ref : #59

--- src/components/user/addUserModal.js
+++ src/components/user/addUserModal.js
@@ -4,11 +4,20 @@ import { Button, Col, Form, Modal, Row } from "react-bootstrap";
 
 /** 사용자 정보 등록 모달 */
 export default function InsertModal(props) {
-    const { onHide, onUserAdded } = props
+    const { onHide, onUserAdded, pageInfoList } = props
     const [Rank, setRank] = useState([])
     const labelStyle = {
         textAlign: 'right'
     };
+
+    const [formData, setFormData] = useState({
+        id: "",
+        password: "",
+        userName: "",
+        regdate: ""
+    });
+    const [errorMessage, setErrorMessage] = useState("");
+
     const getRank = () => {
         axios.post("/api/common/child", { "code_id": 3000 },
             { headers: { "Content-Type": "application/json" } })
@@ -19,6 +28,7 @@ export default function InsertModal(props) {
                 console.log("직급 리스트: " + error)
             })
     }
+    
     const userInput = (e) => {
         e.preventDefault();
         const url = "/api/user/add";
@@ -29,10 +39,55 @@ export default function InsertModal(props) {
                 onHide();
                 onUserAdded();
             })
+            .catch(error => {
+                console.error("사용자 추가 오류:", error);
+                setErrorMessage("사용자 추가 중 오류가 발생했습니다.");
+            });
     }
+
     useEffect(() => {
+        setFormData({
+            id: "",
+            password: "",
+            userName: "",
+            regdate: ""
+        });
+        setErrorMessage("")
         getRank()
-    }, [])
+        console.log(pageInfoList)
+    }, [props.show])
+
+    const existedId = pageInfoList.map(user => user.id);
+
+    const handleAddUserId = (e) => {
+        e.preventDefault();
+        // 새로운 아이디가 이미 존재하는지 확인
+        if (existedId.includes(formData.id)) {
+            setErrorMessage("이미 존재하는 아이디입니다.");
+        } else {
+            // 존재하지 않으면 사용자 추가 수행
+            userInput(e);
+        }
+    }
+
+    const isFormValid = () => {
+        const validate = {};
+        validate.id = !/^[a-zA-Z0-9]{4,20}$/.test(formData.id)
+        validate.password = !(formData.password.length >= 4 && formData.password.length <= 8)
+        validate.userName = !(formData.userName.length <= 5)
+        validate.regdate = !formData.regdate
+        return Object.values(validate).every(val => !val)
+    }
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        })
+    }
+
+
     return (
         <Modal
             {...props}
@@ -41,20 +96,21 @@ export default function InsertModal(props) {
             dialogClassName="modal-90w"
             aria-labelledby="example-custom-modal-styling-title"
         >
-            <Form onSubmit={(e) => userInput(e)}>
+            <Form onSubmit={handleAddUserId}>
                 <Modal.Header closeButton>
                     <Modal.Title id="example-custom-modal-styling-title"></Modal.Title>
                 </Modal.Header>
                 <Modal.Body >
+                    {errorMessage && <p style={{ color: 'red', marginLeft:"50px" }}>{errorMessage}</p>}
                     <Form.Group as={Row} className="mb-3">
                         <Form.Label style={labelStyle} column md="2"> 아이디 : </Form.Label>
-                        <Col md="3"><Form.Control type='text' name='id' /></Col>
+                        <Col md="3"><Form.Control type='text' name='id' value={formData.id} onChange={handleChange}/></Col>
                         <Form.Label style={labelStyle} column md="2"> 비밀번호 : </Form.Label>
-                        <Col md="3"><Form.Control type='password' name='password' /></Col>
+                        <Col md="3"><Form.Control type='password' name='password' value={formData.password} onChange={handleChange}/></Col>
                     </Form.Group>
                     <Form.Group as={Row} className="mb-3">
                         <Form.Label style={labelStyle} column md="2" > 이름 : </Form.Label>
-                        <Col md="3"><Form.Control type='text' name='userName' /></Col>
+                        <Col md="3"><Form.Control type='text' name='userName' value={formData.userName} onChange={handleChange}/></Col>
                         <Form.Label style={labelStyle} column md="2" > 직급 : </Form.Label>
                         <Col md="3">
                             <Form.Select aria-label="직급" name='rank'>
@@ -68,14 +124,14 @@ export default function InsertModal(props) {
                     <Form.Group as={Row}>
                         <Form.Label style={labelStyle} column md="2" type="date" > 입사일 : </Form.Label>
                         <Col md="3">
-                            <input type="date" name="regdate" />
+                            <input type="date" name="regdate" value={formData.regdate} onChange={handleChange}/>
                         </Col>
                         <Form.Label column md="1"></Form.Label>
                         <Col md="5"></Col>
                     </Form.Group>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="success" type="submit">추가</Button>
+                    <Button variant="success" type="submit" disabled={!isFormValid()}>추가</Button>
                     <Button variant="danger" onClick={onHide}>취소</Button>
                 </Modal.Footer>
             </Form>
